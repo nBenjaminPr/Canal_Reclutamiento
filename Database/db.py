0cd12e6b252bf216a47e8425c86da6b7f15bdd19
Nicolas <nicobenpe@gmail.com>
2025-06-05 10:47:51 -0300

    Cambiando carpetas

--- Database/db.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def cargar_datos():
-    ruta = 'C:/Users/Usuario/Desktop/Proyecto/RRHHproyect.xlsx'
-    df = pd.read_excel(ruta)
-    return df
-
-
--- Database/describe.py
+++
@@ -1,9 +0,0 @@
-# Importamos la función del archivo main.py
-from db import cargar_datos
-
-# Cargar el DataFrame
-df = cargar_datos()
-
-# Consultas estadísticas de ejemplo
-print("Resumen estadístico:")
-print(df.describe())
